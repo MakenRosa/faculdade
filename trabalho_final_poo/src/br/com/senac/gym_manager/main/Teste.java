4347d7e0b8a81a67cf096b42ee5793142d182eea
Maken Cristhian <96421658+MakenRosa@users.noreply.github.com>
2022-07-01 10:17:02 -0300

    Quase terminado!

--- trabalho_final_poo/src/br/com/senac/gym_manager/main/Teste.java
+++ trabalho_final_poo/src/br/com/senac/gym_manager/main/Teste.java
@@ -13,82 +13,112 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Teste {
+
+    public static List<Exercicio> criarExercicios() {
+        List<Exercicio> exercicios = new ArrayList();
+        exercicios.add(new Exercicio("Supino declinado", 10, 3, "Halteres"));
+        exercicios.add(new Exercicio("Flexão centrada", 12, 3));
+        exercicios.add(new Exercicio("One Arm Row", 8, 3, "Halteres"));
+        exercicios.add(new Exercicio("Bent Over Row", 8, 3, "Barra"));
+        exercicios.add(new Exercicio("Bícep Curl", 10, 3, "Barra W"));
+        exercicios.add(new Exercicio("Bícep Curl", 8, 3, "Halteres"));
+        exercicios.add(new Exercicio("Trícep Extension", 8, 2, "Barra"));
+        exercicios.add(new Exercicio("Trícep Deep", 8, 3, "Banco"));
+        exercicios.add(new Exercicio("Reverse Fly (deitado)", 8, 3, "Halteres"));
+        exercicios.add(new Exercicio("Lateral Raise", 8, 3, "Polia"));
+        exercicios.add(new Exercicio("Agachamento", 8, 3, "Barra"));
+        exercicios.add(new Exercicio("Abdominal (Oblíquo)", 12, 3));
+        exercicios.add(new Exercicio("Abdominal (Reto)", 12, 3));
+        return exercicios;
+    }
+
+    public static List<Exercicio> listarExercicios(List<Exercicio> lista,
+            int indEx1, int indEx2, int indEx3, int indEx4) throws Exception, IndexOutOfBoundsException {
+        if (lista.size() < 4) {
+            throw new IndexOutOfBoundsException("Lista deve conter pelo menos 4 itens! ");
+        }
+        List<Exercicio> exercicios = new ArrayList();
+        exercicios.add(lista.get(indEx1));
+        exercicios.add(lista.get(indEx2));
+        exercicios.add(lista.get(indEx3));
+        exercicios.add(lista.get(indEx4));
+        return exercicios;
+    }
+
+    public static List<Exercicio> listarExercicios(List<Exercicio> lista,
+            int indEx1, int indEx2, int indEx3, int indEx4, int indEx5) throws Exception, IndexOutOfBoundsException {
+        if (lista.size() < 5) {
+            throw new IndexOutOfBoundsException("Lista deve conter pelo menos 5 itens! ");
+        }
+        List<Exercicio> exercicios = new ArrayList();
+        exercicios.add(lista.get(indEx1));
+        exercicios.add(lista.get(indEx2));
+        exercicios.add(lista.get(indEx3));
+        exercicios.add(lista.get(indEx4));
+        exercicios.add(lista.get(indEx5));
+        return exercicios;
+    }
+    public static void createSetTreinamento(String nomeTreinamento, List<Exercicio>
+            exercicios, DiasDaSemana diaDoTreino, Cliente cliente){
+        Treinamento treino = new Treinamento(nomeTreinamento, exercicios, diaDoTreino);
+        cliente.adicionarTreinamento(treino);
+    }
+    public static void linha(){
+        System.out.println("======================================================");
+    }
+
     public static void main(String[] args) {
         // Instanciando a catraca da academia
         CatracaAcademia catraca = new CatracaAcademia();
-        
-        
-        // Instanciando Exercicios
-        Exercicio supino = new Exercicio("Supino declinado", 10, 3, "Halteres");
-        Exercicio pushUps = new Exercicio("Flexão centrada", 12, 3);
-        Exercicio oneArmRow = new Exercicio("One Arm Row", 8, 3, "Halteres");
-        Exercicio bentOverRow = new Exercicio("Bent Over Row", 8, 3, "Barra");
-        Exercicio curlBarra = new Exercicio("Bícep Curl", 10, 3, "Barra W");
-        Exercicio curlHalter = new Exercicio("Bícep Curl", 8, 3);
-        Exercicio tricepExtension = new Exercicio("Trícep Extension", 8, 2, "Barra");
-        Exercicio tricepDeep = new Exercicio("Trícep Deep", 8, 3, "Banco");
-        Exercicio reverseFly = new Exercicio("Reverse Fly (deitado)", 8, 3, "Halteres");
-        Exercicio lateralRaise = new Exercicio("Lateral Raise", 8, 3, "Polia");
-        Exercicio agachamentoBarra = new Exercicio("Agachamento", 8, 3, "Barra");
-        Exercicio abdominal1 = new Exercicio("Abdominal (Oblíquo)", 12, 3);
-        Exercicio abdominal2 = new Exercicio("Abdominal (Reto)", 12, 3);
-        
-        
-        // Criando e Instanciando Treinamentos
-        List<Exercicio> listTreino1 = new ArrayList();
-        listTreino1.add(pushUps);
-        listTreino1.add(supino);
-        listTreino1.add(curlBarra);
-        listTreino1.add(curlHalter);
-        List<Exercicio> listTreino2 = new ArrayList();
-        listTreino2.add(agachamentoBarra);
-        listTreino2.add(reverseFly);
-        listTreino2.add(lateralRaise);
-        listTreino2.add(abdominal2);
-        List<Exercicio> listTreino3 = new ArrayList();
-        listTreino3.add(abdominal1);
-        listTreino3.add(tricepDeep);
-        listTreino3.add(tricepExtension);
-        listTreino3.add(bentOverRow);
-        listTreino3.add(oneArmRow);
-        List<Exercicio> listTreino4 = new ArrayList();
-        listTreino4.add(tricepDeep);
-        listTreino4.add(curlBarra);
-        listTreino4.add(abdominal2);
-        listTreino4.add(tricepExtension);
-        
-        Treinamento makenSegunda = new Treinamento("Peitoral e bíceps", 
-                listTreino1, DiasDaSemana.SEGUNDA_FEIRA);
-        Treinamento makenQuarta = new Treinamento("Deltoide, quadriceps e abdomen",
-                listTreino2, DiasDaSemana.QUARTA_FEIRA);
-        Treinamento makenSexta = new Treinamento("Abdomen, triceps e dorsal", 
-                listTreino3, DiasDaSemana.SEXTA_FEIRA);
-        Treinamento experimental = new Treinamento("Triceps, biceps e abdomen", 
-                listTreino4);
-        
-        // Instanciando e Adicionando a mensalidade do cliente
-        Mensalidade julho = new Mensalidade("Julho", 125.00, StatusPagamento.PAGAMENTO_EM_ESPERA);
-        
-        // Instanciando pessoas
+        linha();
 
+        // Instanciando a mensalidade do cliente
+        Mensalidade julho = new Mensalidade("Julho", 125.00, StatusPagamento.PAGAMENTO_EM_ESPERA);
+        linha();
+
+        // Instanciando pessoas
         Cliente maken = new Cliente(02101, "Maken da Rosa", "78969647990", 19, julho);
 
         ClienteExperimental leonardo = new ClienteExperimental("Leonardo Fulano",
-                "78933245888", 17);
+                                                            "78933245888", 17);
         leonardo.setDataAulaExperimental("07/07/2022");
-        leonardo.setTreinamento(experimental);
+        linha();
         
-        // Adicionando os Treinamentos as Pessoas
-        maken.adicionarTreinamento(makenSegunda);
-        maken.adicionarTreinamento(makenQuarta);
-        maken.adicionarTreinamento(makenSexta);
-        
-        // Instanciando e Adicionando Cartao de Passe do Cliente na catraca
+        // Instanciando Exercicios
+        List<Exercicio> exercicios = criarExercicios();
+        linha();
+        linha();
+        // Criando listas de exercícios separadas
+        try {
+            List<Exercicio> listTreino1 = listarExercicios(exercicios, 1, 0, 4, 5);
+            List<Exercicio> listTreino2 = listarExercicios(exercicios, 10, 8, 9, 12);
+            List<Exercicio> listTreino3 = listarExercicios(exercicios, 11, 7, 6, 3, 2);
+            List<Exercicio> listTreino4 = listarExercicios(exercicios, 7, 4, 12, 6);
+            // Criando treinamentos e associando-os aos clientes
+            createSetTreinamento("Peitoral e bíceps",
+                    listTreino1, DiasDaSemana.SEGUNDA_FEIRA, maken);
+            createSetTreinamento("Deltoide, quadriceps e abdomen",
+                    listTreino2, DiasDaSemana.QUARTA_FEIRA, maken);
+            createSetTreinamento("Abdomen, triceps e dorsal",
+                    listTreino3, DiasDaSemana.SEXTA_FEIRA, maken);
+            linha();
+            Treinamento experimental = new Treinamento("Triceps, biceps e abdomen",
+                    listTreino4);
+            leonardo.setTreinamento(experimental);
+
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Índices inválidos! " + ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println("Não foi possível criar/associar os treinamentos! " + ex.getMessage());
+        }
+        linha();
+        // Instanciando e Adicionando Cartao na catraca
         CartaoPasseCliente cartaoMaken = new CartaoPasseCliente("451A32X5", catraca);
         catraca.adicionarCartao(cartaoMaken);
+        linha();
+        // Associando cartão ao cliente
         maken.setCartao(cartaoMaken);
-        
-        
+        linha();
         // Utilizando o sistema
         maken.pagarMensalidade();
         maken.entrarNaAcademia();
