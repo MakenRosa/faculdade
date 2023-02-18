94d40d79c082f0054ba9537fdabb36b02cc8dbce
MakenRosa <maken.cristhian@gmail.com>
2023-02-18 03:28:05 -0300

    Implementando Lombok para teste

--- DESENVOLVIMENTO DESKTOP 3a fase/Senac_hibernate/src/main/java/br/com/senac/entidade/Cliente.java
+++ DESENVOLVIMENTO DESKTOP 3a fase/Senac_hibernate/src/main/java/br/com/senac/entidade/Cliente.java
@@ -7,19 +7,20 @@ package br.com.senac.entidade;
 
 import java.io.Serializable;
 import javax.persistence.*;
+import lombok.Data;
 
 /**
  *
  * @author Maken.Rosa
  */
+@Data
 @Entity
 @Table(name = "cliente")
 public class Cliente implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)    private Long id;
 
     @Column(nullable = false)
     private String nome;
@@ -47,74 +48,4 @@ public class Cliente implements Serializable {
         this.rg = rg;
         this.salario = salario;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
-    }
-
-    public Profissao getProfissao() {
-        return profissao;
-    }
-
-    public void setProfissao(Profissao profissao) {
-        this.profissao = profissao;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (!(object instanceof Cliente)) {
-            return false;
-        }
-        Cliente other = (Cliente) object;
-        return !((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)));
-    }
-
-    @Override
-    public String toString() {
-        return "br.com.senac.entidade.Cliente[ id=" + id + " ]";
-    }
-
 }
--- DESENVOLVIMENTO DESKTOP 3a fase/Senac_hibernate/src/main/java/br/com/senac/entidade/Perfil.java
+++ DESENVOLVIMENTO DESKTOP 3a fase/Senac_hibernate/src/main/java/br/com/senac/entidade/Perfil.java
@@ -7,11 +7,13 @@ package br.com.senac.entidade;
 
 import java.io.Serializable;
 import javax.persistence.*;
+import lombok.Data;
 
 /**
  *
  * @author Maken.Rosa
  */
+@Data
 @Entity
 @Table(name = "perfil")
 public class Perfil implements Serializable {
@@ -36,65 +38,6 @@ public class Perfil implements Serializable {
     public Perfil(String nome, String descricao) {
         this.nome = nome;
         this.descricao = descricao;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public boolean isAtivo() {
-        return ativo;
-    }
-
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
-    }
-    
-    
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Perfil)) {
-            return false;
-        }
-        Perfil other = (Perfil) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "br.com.senac.entidade.Perfil[ id=" + id + " ]";
     }
 
 }
