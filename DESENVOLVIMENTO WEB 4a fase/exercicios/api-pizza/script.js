d166018a9a6c72c969673ce74b6abab0ff43fba0
MAKEN CRISTHIAN FARIA DA ROSA <maken.rosa@alunos.sc.senac.br>
2023-04-19 20:50:10 -0300

    Atividade API-pizza dev web

---
+++ DESENVOLVIMENTO WEB 4a fase/exercicios/api-pizza/script.js
@@ -0,0 +1,21 @@
+fetch('https://raw.githubusercontent.com/LenaLatosinski/cardapio.json/main/cardapio.json')
+	.then(response => response.json())
+	.then(data => {
+		console.log(data)
+		const cardapio = document.getElementById('cardapio');
+		data.cardapio.forEach(prato => {
+			const pratoElement = document.createElement('div');
+			pratoElement.classList.add('prato');
+			pratoElement.innerHTML = `
+				<img src="${prato.image}" onerror="this.onerror=null;this.src='https://via.placeholder.com/150';">
+				<h2>${prato.name}</h2>
+				<p>${prato.description}</p>
+				<p>${prato.price}</p>
+				<p>${prato.gluten}</p>
+				<p>${prato.calories}</p>
+				<button>Adicionar ao carrinho</button>
+			`;
+			cardapio.appendChild(pratoElement);
+		});
+	})
+	.catch(error => console.error(error));
