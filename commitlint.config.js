5d73e3b6dd40597f340397d5ac12b0577dda0189
MakenRosa <maken.cristhian@gmail.com>
2023-05-29 11:31:11 -0300

    chore(core): configured commitlint and husky

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-  };
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test']
+    ],
+    'scope-empty': [2, 'never'],
+    'scope-enum': [
+      2,
+      'always',
+      ['aoo', 'assgi', 'crud', 'dev-desktop', 'dev-web', 'pig', 'poo', 'qa', 'sgbd', 'core']
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-case': [2, 'never', ['upper-case']],
+  }
+};
